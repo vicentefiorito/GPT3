b53611a51a96bd7a308138806e0da1fafd1b9b1f
vicentefiorito <vicentefiorito@gmail.com>
2023-01-03 14:35:21 -0500

    Made button and did the styling for it

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -10,13 +10,17 @@ const NavBar = () => {
         <div className='gpt3__navbar-links_logo'>
           <img src={logo} alt="logo" />
         </div>
-        <div className='gpt3__navbar-links_containter'>
+        <div className='gpt3__navbar-links_container'>
           <p><a href='#home'>Home</a></p>
           <p><a href='#wgpt3'>What is GPT3</a></p>
           <p><a href='#possibility'>Open AI</a></p>
           <p><a href='features'>Create Studies</a></p>
           <p><a href='blog'>Library</a></p>
         </div>
+      </div>
+      <div className='gpt3__navbar-sign'>
+        <p>Sign in</p>
+        <button type='button'>Sign up</button>
       </div>
     </div>
   )
