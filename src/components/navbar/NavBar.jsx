d7037403b70449204a33893486b8555a063551f6
vicentefiorito <vicentefiorito@gmail.com>
2023-01-03 14:19:38 -0500

    Added the rest of the menu to navBar

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -10,6 +10,13 @@ const NavBar = () => {
         <div className='gpt3__navbar-links_logo'>
           <img src={logo} alt="logo" />
         </div>
+        <div className='gpt3__navbar-links_containter'>
+          <p><a href='#home'>Home</a></p>
+          <p><a href='#wgpt3'>What is GPT3</a></p>
+          <p><a href='#possibility'>Open AI</a></p>
+          <p><a href='features'>Create Studies</a></p>
+          <p><a href='blog'>Library</a></p>
+        </div>
       </div>
     </div>
   )
